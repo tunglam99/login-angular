c5dc798da8d126df197408098d65dbed179d3450
tunglam99 <lamnthnue.edu.vn@gmail.com>
2019-12-12 10:08:15 +0700

    fix api

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -2,10 +2,10 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 
 import {User} from '../model/user';
-import {environment} from '../../environments/environment';
 import {Observable} from 'rxjs';
+import {environment} from '../../environments/environment';
 
-const API_URL = environment.apiUrl;
+const API_URL = `${environment.apiUrl}`;
 
 @Injectable({
   providedIn: 'root'
@@ -15,6 +15,6 @@ export class UserService {
   constructor(private http: HttpClient) {
   }
   register(user: User): Observable<User> {
-    return this.http.post <User> (API_URL + '/register', user);
+    return this.http.post<User>(API_URL + '/register', user);
   }
 }
